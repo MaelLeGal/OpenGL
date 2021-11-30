1318f4b367f4d735039b1dcf6f40bb982e0e320b
MaelLeGal <mael.legal@live.fr>
2021-11-30 12:02:57 +0100

    Added vertex  structure and usage of texture.

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -3,14 +3,30 @@
 #include <optional>
 #include <functional>
 #include <iostream>
+#include <cstddef>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include "loadShader.h"
+#include "stb_image.h";
 
 using namespace std;
 
+struct vertex_struct {
+
+	GLfloat vertexbuffer[3];
+	GLfloat color[3];
+	GLfloat uv[2];
+	GLfloat normal[3];
+	GLfloat texCoord[2];
+};
+
+static const std::vector<vertex_struct> vertices{
+	{{0.0f,0.0f,0.0f}, {1.0f,0.0f,0.0f}, {0.0f,0.0f}, {0.0f,0.0f,-1.0f},{0.0f,0.0f}},
+	{{1.0f,0.0f,0.0f}, {0.0f,1.0f,0.0f}, {1.0f,0.0f}, {0.0f,0.0f,-1.0f},{1.0f,0.0f}},
+	{{0.0f,1.0f,0.0f}, {0.0f,0.0f,1.0f}, {0.0f,1.0f}, {0.0f,0.0f,-1.0f},{0.0f,1.0f}}
+};
 
 static const GLfloat g_vertex_buffer_data[] = {
 	0.0f, 0.0f, 0.0f,
@@ -50,6 +66,19 @@ optional<GLFWwindow*> Setup() {
 	return window;
 }
 
+/*template<typename Type> void EnableAttrib(char* name, GLuint programID, int size) {
+	GLint location = glGetAttribLocation(programID, name);
+	glVertexAttribPointer(
+		location,					// attribute 0. No particular reason for 0, but must match the layout in the shader.
+		size,					// size
+		GL_FLOAT,			// type
+		GL_FALSE,			// normalized?
+		sizeof(Type),					// stride
+		(void*)offsetof(Type, Type::vertexbuffer)			// array buffer offset
+	);
+	glEnableVertexAttribArray(location);
+}*/
+
 int main(int argc, char* argv[]) {
 
 	// Setup
@@ -70,7 +99,8 @@ int main(int argc, char* argv[]) {
 	GLuint vertexBuffer;
 	glGenBuffers(1, &vertexBuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_struct)*vertices.size(), vertices.data(), GL_STATIC_DRAW);
 
 	GLuint programID = LoadShaders("../shaders/TestVertexShader.shader", "../shaders/TestFragmentShader.shader");
 
@@ -80,8 +110,33 @@ int main(int argc, char* argv[]) {
 	GLuint scale_frag_uniform_location = glGetUniformLocation(programID, "scale_frag");
 	GLuint width_uniform_location = glGetUniformLocation(programID, "width");
 	GLuint height_uniform_location = glGetUniformLocation(programID, "height");
+	GLuint mode_location = glGetUniformLocation(programID, "mode");
 
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	int mode = 0;
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	GLuint textures;
+	if (data) {
+		glCreateTextures(GL_TEXTURE_2D, 1, &textures);
+		glTextureStorage2D(textures, 1, GL_RGB8, width, height);
+		glTextureSubImage2D(textures, 0, 0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+		glBindTextureUnit(0, textures);
+		GLuint texture_color_location = glGetUniformLocation(programID, "colorTexture");
+		glUniform1i(texture_color_location, 0);
+	}
+	else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	
+
+	stbi_image_free(data);
+
+	// Start loop
 	do {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -93,23 +148,86 @@ int main(int argc, char* argv[]) {
 
 		t = glfwGetTime();
 		float scale_uniform = std::sin(t);
+
+		if(glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
+			mode = 0;
+		}
+		else if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
+			mode = 1;
+		}
+		else if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS) {
+			mode = 2;
+		}
+		else {
+			mode = -1;
+		}
+
 		glUniform1f(scale_uniform_location, scale_uniform);
 		glUniform1f(scale_frag_uniform_location, scale_uniform);
 		glUniform1i(width_uniform_location, w);
 		glUniform1i(height_uniform_location, h);
+		glUniform1i(mode_location, mode);
 
 
 		//Loop body // Draw here
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+		GLint positionLocation = glGetAttribLocation(programID, "vertexPosition_modelspace");
 		glVertexAttribPointer(
-			0,					// attribute 0. No particular reason for 0, but must match the layout in the shader.
+			positionLocation,					// attribute 0. No particular reason for 0, but must match the layout in the shader.
 			3,					// size
 			GL_FLOAT,			// type
 			GL_FALSE,			// normalized?
-			0,					// stride
-			(void*)0			// array buffer offset
+			sizeof(vertex_struct),					// stride
+			(void*)offsetof(vertex_struct, vertex_struct::vertexbuffer)			// array buffer offset
 		);
+		glEnableVertexAttribArray(positionLocation);
+
+		//EnableAttrib<vertex_struct>("vertexPosition_modelspace", programID, 3);
+
+		GLint colorLocation = glGetAttribLocation(programID, "color");
+		glVertexAttribPointer(
+			colorLocation,					// attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,					// size
+			GL_FLOAT,			// type
+			GL_FALSE,			// normalized?
+			sizeof(vertex_struct),					// stride
+			(void*)offsetof(vertex_struct, vertex_struct::color)			// array buffer offset
+		);
+		glEnableVertexAttribArray(colorLocation);
+
+		GLint uvLocation = glGetAttribLocation(programID, "uv");
+		glVertexAttribPointer(
+			uvLocation,					// attribute 0. No particular reason for 0, but must match the layout in the shader.
+			2,					// size
+			GL_FLOAT,			// type
+			GL_FALSE,			// normalized?
+			sizeof(vertex_struct),					// stride
+			(void*)offsetof(vertex_struct, vertex_struct::uv)			// array buffer offset
+		);
+		glEnableVertexAttribArray(uvLocation);
+
+		GLint normalLocation = glGetAttribLocation(programID, "normal");
+		glVertexAttribPointer(
+			normalLocation,					// attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,					// size
+			GL_FLOAT,			// type
+			GL_FALSE,			// normalized?
+			sizeof(vertex_struct),					// stride
+			(void*)offsetof(vertex_struct, vertex_struct::normal)			// array buffer offset
+		);
+		glEnableVertexAttribArray(normalLocation);
+
+		GLint textureCoordinatesLocation = glGetAttribLocation(programID, "aTexCoord");
+		glVertexAttribPointer(
+			textureCoordinatesLocation,					// attribute 0. No particular reason for 0, but must match the layout in the shader.
+			2,					// size
+			GL_FLOAT,			// type
+			GL_FALSE,			// normalized?
+			sizeof(vertex_struct),					// stride
+			(void*)offsetof(vertex_struct, vertex_struct::texCoord)			// array buffer offset
+		);
+		glEnableVertexAttribArray(textureCoordinatesLocation);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
 		glDisableVertexAttribArray(0);
--- shaders/TestFragmentShader.shader
+++ shaders/TestFragmentShader.shader
@@ -1,11 +1,32 @@
 #version 450 core
-out vec3 color;
+
+in vec3 color_out;
+in vec2 uv_out;
+in vec3 normal_out;
+in vec2 TexCoord;
+
+out vec4 color;
 
 uniform float scale_frag;
 uniform int width;
 uniform int height;
+uniform int mode;
+uniform sampler2D colorTexture;
 
 void main()
 {
-	color = vec3(abs(scale_frag)*(gl_FragCoord.x/width), abs(scale_frag)*(gl_FragCoord.y/height), abs(scale_frag)*1);
+	//color = vec4(abs(scale_frag)*(gl_FragCoord.x/width), abs(scale_frag)*(gl_FragCoord.y/height), abs(scale_frag)*1,1);
+	if (mode == 0) {
+		color = vec4(color_out, 1);
+	}
+	else if (mode == 1) {
+		color = vec4(normal_out, 1);
+	}
+	else if (mode == 2) {
+		color = vec4(uv_out, 1, 1);
+	}
+	else {
+		//color = vec4(abs(scale_frag) * (gl_FragCoord.x / width), abs(scale_frag) * (gl_FragCoord.y / height), abs(scale_frag) * 1, 1);
+		color = texture(colorTexture, TexCoord);	
+	}
 }
